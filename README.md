3992c960b4edaf47ffbf76fab8566af5cba9ee34
John Reynolds <john@johnreynolds3d.com>
2022-06-17 13:22:39 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# BatesMansion
+
+Developed with Unreal Engine 5.
---
+++ Source/BatesMansion/BatesMansion.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BatesMansion.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BatesMansion, "BatesMansion" );
